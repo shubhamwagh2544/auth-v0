9069978706d331116d33cc3a9512df7233660848
shubham.wagh <shubhamwagh2544@gmail.com>
2024-03-14 23:40:40 +0530

    header component added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,8 +10,8 @@ function App() {
 
   return (
     <>
-      <Header />
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,22 @@
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
-        <div>Header</div>
+        <div className="flex bg-slate-200 justify-between p-3">
+            <Link to="/">
+                <div className="text-2xl font-semibold ml-10">Auth App</div>
+            </Link>
+            <ul className="flex gap-8 mr-10">
+                <Link to="/">
+                    <li>Home</li>
+                </Link>
+                <Link to="/about">
+                    <li>About</li>
+                </Link>
+                <Link to="/signin">
+                    <li>SignIn</li>
+                </Link>                     {/* Link must be inside Router */}
+            </ul>
+        </div>
     )
 }
