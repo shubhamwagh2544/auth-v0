9d0a854c7ef0c34451794d2330f1d1b03fb92491
shubham.wagh <shubhamwagh2544@gmail.com>
2024-03-14 20:15:32 +0530

    router pages structure created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import SignIn from './pages/Signin'
+import SignUp from './pages/Signup'
+import Profile from './pages/Profile'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
