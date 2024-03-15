f68eae581fd6708e1f62df674bdc2b3a70e7bd44
shubham.wagh <shubhamwagh2544@gmail.com>
2024-03-15 10:01:18 +0530

    signup route added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
-import express from 'express'
 import mongoose from 'mongoose'
+import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import { User } from './db.js'
 const app = express()
 const PORT = 3000
 dotenv.config()
@@ -9,5 +11,36 @@ mongoose
     .connect(process.env.MONGO)
     .then(() => console.log('connected to mongodb'))
     .catch((err) => console.log(err))
+
+app.use(express.json())
+app.use(cors())
+
+// routes
+app.post('/signup', async (req, res) => {
+    const { username, email, password } = req.body
+    try {
+        const user = new User({
+            username,
+            email,
+            password
+        })
+        await user.save()
+        return res.status(201).json({
+            message: 'user created successfully'
+        })
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+})
+
+
+app.all('*', (req, res) => {
+    return res.json({
+        msg: 'home'
+    })
+})
 
 app.listen(PORT, () => console.log(`server started on port: ${PORT}`))
