84d11b2bbda4ba34edbd63b58596598b24211202
shubham.wagh <shubhamwagh2544@gmail.com>
2024-03-15 10:22:06 +0530

    global and custom error handler added

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose'
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import bcrypt from 'bcrypt'
 import { User } from './db.js'
+import errorHandler from './error.js'
 const app = express()
 const PORT = 3000
 dotenv.config()
@@ -16,13 +18,17 @@ app.use(express.json())
 app.use(cors())
 
 // routes
-app.post('/signup', async (req, res) => {
+app.post('/signup', async (req, res, next) => {
     const { username, email, password } = req.body
+
+    // password hash
+    const hashedPassword = bcrypt.hashSync(password, 10)       // 10 salt rounds
+
     try {
         const user = new User({
             username,
             email,
-            password
+            password: hashedPassword
         })
         await user.save()
         return res.status(201).json({
@@ -30,16 +36,30 @@ app.post('/signup', async (req, res) => {
         })
     }
     catch (err) {
-        return res.status(500).json({
-            message: err.message
-        })
+        // return res.status(500).json({
+        //     message: err.message
+        // })
+        next(err)
     }
 })
 
 
+// global route handler
 app.all('*', (req, res) => {
     return res.json({
-        msg: 'home'
+        msg: 'this might not be the page you are looking for!!!'
+    })
+})
+
+// global error handler
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+
+    return res.status(statusCode).json({
+        success: false,
+        error: message,
+        statusCode
     })
 })
 
