6b48781055c24e9b55b7189aad5b3071b9ea4ffd
shubham.wagh <shubhamwagh2544@gmail.com>
2024-03-16 23:41:44 +0530

    private route logic added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from './pages/Signin'
 import SignUp from './pages/Signup'
 import Profile from './pages/Profile'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
 
@@ -17,7 +18,9 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/signin' element={<SignIn />} />
           <Route path='/signup' element={<SignUp />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
