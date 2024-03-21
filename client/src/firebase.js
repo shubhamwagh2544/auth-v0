ed2c6c01a700f19d847b3e5698604f9e2ec77524
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-21 10:44:16 +0530

    preproduction changes-1

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
+    authDomain: `${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN}`,
     projectId: "auth-v0-7cac6",
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    storageBucket: `${import.meta.env.VITE_FIREBASE_STORAGE_BUCKET}`,
     messagingSenderId: "1053640377170",
     appId: "1:1053640377170:web:4e1797a231c92143c0f08b"
 };
